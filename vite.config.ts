f65ec2651a3a7bf260df93886310f775030cf1b8
Frost <murtada.najm99@gmail.com>
2026-04-02 14:18:42 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test-for-wagitly/',
+  base: '/test-wagitly/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
